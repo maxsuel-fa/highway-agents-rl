db44c72d32ce6f312539a442226ae3434352723b
maxsuel-fa <maxsuell.fernandes@gmail.com>
2025-04-14 14:34:05 -0400

    config: Adding some simple configuration files for the environment

---
+++ config/simpleconfig.py
@@ -0,0 +1,29 @@
+import os
+import pickle
+
+config_dict = {
+    "observation": {
+        "type": "OccupancyGrid",
+        "vehicles_count": 10,
+        "features": ["presence", "x", "y", "vx", "vy", "cos_h", "sin_h"],
+        "features_range": {
+            "x": [-100, 100],
+            "y": [-100, 100],
+            "vx": [-20, 20],
+            "vy": [-20, 20],
+        },
+        "grid_size": [[-20, 20], [-20, 20]],
+        "grid_step": [5, 5],
+        "absolute": False,
+    },
+    "lanes_count": 4,
+    "vehicles_count": 15,
+    "duration": 40,  # [s]
+}
+
+
+curr_dir = os.path.dirname(os.path.realpath(__file__))
+config_path = os.path.join(curr_dir, 'simple_config.pkl')
+with open(config_path, "wb") as f:
+    pickle.dump(config_dict, f)
+
